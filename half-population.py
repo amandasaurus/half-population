13a5223d2c355f1392cb133515b9dc086a943654
Rory McCann <rory@technomancy.org>
2014-03-18 22:08:54 +0000

    Print statistics of how much was actually covered

--- half-population.py
+++ half-population.py
@@ -51,22 +51,26 @@ def allocate_shapes(shapes, input_column_name, output_column_name):
     half = total / 2
     so_far = 0
     first_half_ids = set()
+
     for shape in sorted(shapes, key=lambda shape: (shape['properties'][input_column_name] / shapely.geometry.shape(shape['geometry']).area) ):
         so_far += shape['properties'][input_column_name]
         shape['properties'][output_column_name] = 1
         if so_far > half:
             break
 
-    return shapes
+    return shapes, total, so_far
 
-
+def print_result_stats(total, first_half):
+    second_half = total - first_half
+    print "There is {:,} in total, and {:,} ({:5.2%}) in first half, {:,} ({:5.2%}) in second half".format(total, first_half, first_half/total, second_half, second_half/total)
 
 
 def main(argv):
     args = parse_args(argv)
     shapes, fileformat_meta = open_input_source(args.input)
     shapes, fileformat_meta = add_output_column(shapes, fileformat_meta, args.output_column)
-    shapes = allocate_shapes(shapes, args.property_to_half, args.output_column)
+    shapes, total, first_half = allocate_shapes(shapes, args.property_to_half, args.output_column)
+    print_result_stats(total, first_half)
     write_shapefile(args.output, shapes, fileformat_meta)
 
 if __name__ == '__main__':
